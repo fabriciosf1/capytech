a39402689e59d4cda7251b1d2a065f0ee3b8a691
Fabricio Ferreira <fabriciosf@gmail.com>
2026-02-05 17:58:02 -0300

    Alteração para webhook para n8n.capitech

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -84,10 +84,10 @@ const loading = ref(false)
 
 // --- CONFIGURAÇÃO DE ENVIO ---
 // Mude para true quando quiser usar o n8n
-const USE_WEBHOOK = false
+const USE_WEBHOOK = true
 
 // Configuração Webhook (n8n/Make)
-const WEBHOOK_URL = 'http://localhost:5678/webhook-test/23bc4c9c-d8cc-48f2-8688-0b0179017be1'
+const WEBHOOK_URL = 'https://n8n.capitech.ia.br/webhook-test/23bc4c9c-d8cc-48f2-8688-0b0179017be1'
 
 
 // Configuração EmailJS
